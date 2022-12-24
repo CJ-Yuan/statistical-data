ce1bfe0ddd7f3dcdeb32e1a7a58909a78b38bac3
CJ-Yuan <55149541+CJ-Yuan@users.noreply.github.com>
2022-12-25 02:54:16 +0800

    Add files via upload

--- Server/db/index.js
+++ Server/db/index.js
@@ -1,26 +1,12 @@
-// // 导入 mysql 模块
-// const mysql = require('mysql')
-
-// // 创建数据库连接对象
-// const db = mysql.createPool({
-//     host: 'localhost', //Node 部署:127.0.0.1 本地：localhost
-//     user: 'root', 
-//     password: 'yuan520',
-//     database: 'data_yuan',
-//   })
-
-// // 向外共享 db 数据库连接对象
-// module.exports = db
-
 // 导入 mysql 模块
 const mysql = require('mysql')
 
 // 创建数据库连接对象
 const db = mysql.createPool({
-    host: '127.0.0.1', //Node 部署:127.0.0.1 本地：localhost
-    user: '	yuan_tong', 
-    password: 'YtKB7xfReDntwdAm',
-    database: 'yuan_tong',
+    host: 'localhost', //Node 部署:127.0.0.1 本地：localhost
+    user: 'root', 
+    password: 'yuan520',
+    database: 'data_yuan',
   })
 
 // 向外共享 db 数据库连接对象
